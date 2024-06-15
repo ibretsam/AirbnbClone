2ed82eba79fb7fcb5315de30fc38fee09d205210
Khanh Le <masterbruculous@gmail.com>
2024-06-15 12:26:01 +0700

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by MasterBi on 15/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
