a0ed29fc1fc1ec01ca3dcb1cf45ffe3ca49df78d
Khanh Le <masterbruculous@gmail.com>
2024-06-16 22:34:25 +0700

    Listing Data Model

---
+++ AirbnbClone/Extensions/DeveloperPreview.swift
@@ -0,0 +1,101 @@
+//
+//  DeveloperPreview.swift
+//  AirbnbClone
+//
+//  Created by MasterBi on 16/6/24.
+//
+
+import Foundation
+
+class DeveloperPreview {
+    var listings: [Listing] = [
+        .init(
+            id: NSUUID().uuidString,
+            ownerUid: NSUUID().uuidString,
+            ownerName: "John Smith",
+            ownerImageUrl: "male-profile-photo",
+            numberOfBedrooms: 4,
+            numberOfBathrooms: 3,
+            numberOfGuests: 4,
+            numberOfBeds: 4,
+            pricePerNight: 567,
+            latitude: 25.7850,
+            longitude: -80.1936,
+            imageURLS: ["listing-1", "listing-2", "listing-3", "listing-4"],
+            address: "124 Main St",
+            city: "Miami",
+            state: "Florida",
+            title: "Miami Villa",
+            rating: 4.86,
+            features: [.selfCheckIn, .superHost],
+            amenities: [.wifi, .alarmSystem, .balcony, .laundry, .tv],
+            type: .villa
+        ),
+        .init(
+            id: NSUUID().uuidString,
+            ownerUid: NSUUID().uuidString,
+            ownerName: "John Smith",
+            ownerImageUrl: "male-profile-photo",
+            numberOfBedrooms: 4,
+            numberOfBathrooms: 3,
+            numberOfGuests: 4,
+            numberOfBeds: 4,
+            pricePerNight: 567,
+            latitude: 25.7850,
+            longitude: -80.1936,
+            imageURLS: ["listing-2", "listing-3", "listing-4", "listing-1"],
+            address: "124 Main St",
+            city: "Miami",
+            state: "Florida",
+            title: "Miami Beach House",
+            rating: 4.86,
+            features: [.selfCheckIn, .superHost],
+            amenities: [.wifi, .alarmSystem, .balcony, .laundry, .tv],
+            type: .villa
+        ),
+        .init(
+            id: NSUUID().uuidString,
+            ownerUid: NSUUID().uuidString,
+            ownerName: "Steve Johnson",
+            ownerImageUrl: "male-profile-photo",
+            numberOfBedrooms: 4,
+            numberOfBathrooms: 3,
+            numberOfGuests: 4,
+            numberOfBeds: 4,
+            pricePerNight: 763,
+            latitude: 25.7850,
+            longitude: -80.1936,
+            imageURLS: ["listing-3", "listing-4", "listing-1", "listing-2"],
+            address: "124 Main St",
+            city: "Miami",
+            state: "Florida",
+            title: "Beautiful Miami apartment in downtown Brickell",
+            rating: 4.86,
+            features: [.selfCheckIn, .superHost],
+            amenities: [.wifi, .alarmSystem, .balcony],
+            type: .apartment
+        ),
+        .init(
+            id: NSUUID().uuidString,
+            ownerUid: NSUUID().uuidString,
+            ownerName: "Harry Styles",
+            ownerImageUrl: "male-profile-photo",
+            numberOfBedrooms: 4,
+            numberOfBathrooms: 3,
+            numberOfGuests: 4,
+            numberOfBeds: 4,
+            pricePerNight: 763,
+            latitude: 34.2,
+            longitude: -118.0426,
+            imageURLS: ["listing-4", "listing-1", "listing-2", "listing-3"],
+            address: "124 Main St",
+            city: "Los Angeles",
+            state: "California",
+            title: "Beautiful Los Angeles home in Malibu",
+            rating: 4.97,
+            features: [.selfCheckIn, .superHost],
+            amenities: [.wifi, .alarmSystem, .pool],
+            type: .apartment
+        ),
+    ]
+}
